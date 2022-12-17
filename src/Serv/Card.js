e1a4e20e5a54a711abc5308b55e8fac56d3a1e8b
AhmedSEHILI <ka_sehili@esi.dz>
2022-12-17 23:57:58 +0100

    services updates

--- src/Serv/Card.js
+++ src/Serv/Card.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-function Card({rotation}) {
+function Card({visibility, image, job, numOffers}) {
     return (
-        <motion.div className={`contain relative border-black w-[160px] h-[210px] ${rotation}`}
+        <motion.div className={`contain relative border-black w-[160px] h-[210px] ${visibility}`}
             whileHover={{scale:1.2}}
             transition={{delay:0.1}}
         >
             <motion.div className="back absolute w-[134px] h-[187px] top-[11px] left-[13px] invisible bg-[#FDD0D2]"
             >
             </motion.div>
-            <img src="/Services/CardBord.svg" alt="" className="absolute w-full h-full" />
+            <img src="/Services/cardBord.svg" alt="" className="absolute w-full h-full" />
             <div className="absolute text-center left-[50%] translate-x-[-50%] top-[16%]">
-                 <h2 className="font-bold text-[#1D3072] text-[23px] pb-2">Building</h2>
-                 <h1 className="font-semibold text-sm">22 offers</h1>
+                 <h2 className="font-bold text-[#1D3072] text-[23px] pb-2">{job}</h2>
+                 <h1 className="font-semibold text-sm">{numOffers} offers</h1>
             </div>
-            <img src="/Services/Builder.svg" alt="" className="absolute left-[50%] translate-x-[-55%] bottom-[5.4%]" />
+            <img src={image} alt="" className="absolute left-[50%] translate-x-[-55%] bottom-[5.4%]" />
         </motion.div>
     );
 }
--- src/Serv/Serv.js
+++ src/Serv/Serv.js
@@ -5,15 +5,29 @@ import Card from './Card';
 
 
 function Serv(props) {
+
+    const cards = [
+        {visibility:"invisible",rotation:"", image:"", job:"", numOffers:"", key:1},
+        {visibility:"",rotation:"rotate-[-20deg]", image:"/Services/Builder.svg", job:"Builder", numOffers:22, key:1},
+        {visibility:"invisible",rotation:"", image:"", job:"", numOffers:"", key:1},
+        {visibility:"",rotation:"rotate-[-20deg]", image:"/Services/painter.svg", job:"Painting", numOffers:30, key:1},
+        {visibility:"invisible",rotation:"", image:"", job:"", numOffers:"", key:1},
+        {visibility:"",rotation:"rotate-[20deg]", image:"/Services/Electricien.svg", job:"Electricity", numOffers:25, key:1},
+        {visibility:"invisible",rotation:"", image:"", job:"", numOffers:"", key:1},
+        {visibility:"",rotation:"rotate-[20deg]", image:"/Services/Mechanicien.svg", job:"Mechanic", numOffers:43, key:1},
+        {visibility:"invisible",rotation:"", image:"", job:"", numOffers:"", key:1},
+        {visibility:"",rotation:"rotate-[20deg]", image:"/Services/Carpenter.svg", job:"Carpentry", numOffers:14, key:1}
+    ]
+
     return (
-        <div className="">
+        <div className="bg-[#FCF9F2]">
             <Bg/>
             <div className="flex flex-col items-center">
                 <h3 className="text-4xl llgg:text-5xl font-bold text-[#1D3072] pt-16 pb-2">Explore services by category</h3>
                 <div className="font-semibold text-sm llgg:text-md w-[400px] pt-2 pb-5 lg:pb-10 text-center">Have you a skill ? propose your services to get the most exiting job in the country !</div>
                 <div className="">
                         <div className="fullGrid grid grid-cols-5 pt-10">
-                            <Card rotation="invisible"/>
+                            {/* <Card rotation="invisible"/>
                             <div className="rot1 rotate-[-20deg]"><Card rotation=""/></div>
                             <Card rotation="invisible"/>
                             <div className="rot1 rotate-[-20deg]"><Card rotation=""/></div>
@@ -22,7 +36,14 @@ function Serv(props) {
                             <Card rotation="invisible"/>
                             <div className="rot1 rotate-[20deg]"><Card rotation=""/></div>
                             <Card rotation="invisible"/>
-                            <div className="rot1 rotate-[20deg]"><Card rotation=""/></div>
+                            <div className="rot1 rotate-[20deg]"><Card rotation=""/></div> */}
+                            {
+                                cards.map((card) => <div className={`rot1 ${card.rotation}`}><Card visibility={card.visibility} 
+                                                                                                image={card.image}
+                                                                                                job={card.job}
+                                                                                                numOffers={card.numOffers}/></div>)
+
+                            }
                         </div>
 
                 </div>            
